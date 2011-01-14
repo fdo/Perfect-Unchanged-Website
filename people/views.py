3a3b3e2f70d261759110f96ca50371a30ad819e8
Fernando Gorchs <fernando.gorchs@yahoo.com>
2011-01-14 15:27:03 -0500

    fixed first page up to view my work

--- people/views.py
+++ people/views.py
@@ -14,11 +14,18 @@ def index(request):
 
 def what(request):
     response = HttpResponse()
-    response.write("<center><H1>what</H1></center><HR>\n")
-    lst = Person.objects.all()
-    for p in lst:
-        link = "<a href=\"/people/Info/%d/\">\n" % p.id
-        response.write("<li>%s%s</a></li>\n" % (link,p.name)) 
+    response.write("<body><html>\n")
+    response.write("<title>database programming is what this is</title>\n")
+    response.write("<center><H3>some django code I whipped up to learn how to access a database from the web</H3></center><HR>\n")
+    link = "<a href=\"/people/\">\n"
+    response.write("<li>%slist of people</a></li>\n" % link) 
+    link = "<a href=\"/fitness/weight/\">\n"
+    response.write("<li>%srecordings of my weight</a></li>\n" % link) 
+    link = "<a href=\"/fitness/running/\">\n"
+    response.write("<li>%sskimpy running diary</a></li>\n" % link) 
+    link = "<a href=\"/fitness/reps/\">\n"
+    response.write("<li>%sexercise</a></li>\n" % link) 
+    response.write("<p>coming soon I will let a SQL query be modified everso slightly (I am worried about SQL injection)<p>\n")
     response.write("</body></html>\n")
     return response
     
